577e08318e9ec4b0ef8447b7edb3778075d1157f
al7771998 <47841102+al7771998@users.noreply.github.com>
2019-06-14 16:28:06 +0800

    update

--- crawling.py
+++ crawling.py
@@ -1,86 +1,52 @@
-#coding=utf-8
-import requests
+# web_scrap.py
+ 
+import concurrent.futures
+import requests                # This is not standard library
+import json
 from bs4 import BeautifulSoup
-import re
-import numpy as np
-import json 
+f = open('urlsPttGossip2.json', 'r')
+URLS = json.load(f)
+f.close()
+ 
 
-HTML_PARSER = "html.parser"
-
+def get_content(url):
+    r = requests.Session()
+    payload = {
+        "from": "/bbs/Gossiping/index.html",
+        "yes": "yes"
+    }
+    r1 = r.post("https://www.ptt.cc/ask/over18?from=%2Fbbs%2FGossiping%2Findex.html", payload)
+    return r.get(url).text
+ 
+ 
+def scrap():
+    docs = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+        future_to_url = {executor.submit(get_content, url): url for url in URLS}
+        for future in concurrent.futures.as_completed(future_to_url):
+            url = future_to_url[future]
+            try:
+                data = future.result()
+            except Execption as exc:
+                print('%r generated an exception: %s' % (url, exc))
+            else:
+                #print('%r data is %s' % (url, data))
+                soup = BeautifulSoup(data, "html.parser")
+                links = soup.find_all("div", class_="bbs-screen bbs-content")
+                for link in links:
+                    doc = link.text
+                    docs.append(doc)
+        with open('ptt_gossip_docs.json', 'w') as outfile:
+            json.dump(docs, outfile)
+def main():
+    for url in URLS:
+        try:
+            data = get_content(url)
+        except Exception as exc:
+            print('%r generated an exception: %s' % (url, exc))
+        else:
+            print('%r page length is %d' % (url, len(data)))
+ 
+ 
 if __name__ == '__main__':
-  url = 'http://www.ptt.cc/bbs/Gossiping/index.html'
-  # over 18 request
-  r = requests.Session()
-  payload = {
-    "from": "/bbs/Gossiping/index.html",
-    "yes": "yes"
-  }
-  r1 = r.post("https://www.ptt.cc/ask/over18?from=%2Fbbs%2FGossiping%2Findex.html", payload)
-  urls = []
-  for i in range(500):
-    req = r.get(url)
-    soup = BeautifulSoup(req.content, HTML_PARSER)
-    #print(soup)
-    titles = soup.find_all('div', class_='title')
-    #print(titles)
-    for t in titles:
-      tag = t.find('a')
-      if tag is None:
-        continue
-      url_str = 'http://www.ptt.cc' + tag.get('href')
-      urls.append(url_str)
-    page_button = soup.find('div', class_='btn-group btn-group-paging')
-    last_page = page_button.find_all('a', class_='btn wide')[1]
-    #print(last_page)
-    url = 'http://www.ptt.cc' + last_page['href']
-  print(urls)
-
-  #存連結用的
-  """with open ('urlsPttGossip2.json','w') as f:
-           json.dump(urls,f)"""
-
-  '''f = open('urlsPttGossip2.json', 'r')
-        urls = json.load(f)
-  f.close()'''
-  #count = 0
-  docs = []
-  r = requests.Session()
-  payload = {
-    "from": "/bbs/Gossiping/index.html",
-    "yes": "yes"
-  }
-  r1 = r.post("https://www.ptt.cc/ask/over18?from=%2Fbbs%2FGossiping%2Findex.html", payload)
-  for url_str in urls: 
-      page = 1 
-      print(count)
-      res = r.get(url_str)
-      soup = BeautifulSoup(res.text, "html.parser")
-      #print(soup)
-      links = soup.find_all("div", class_="bbs-screen bbs-content")
-      #print(links)
-      '''if count <= 20:
-          print(links)
-          print(url_str)
-          for link in links:
-              print(link)
-              print('--------------------')
-              link.text是網站內文
-              print(link.text)
-
-              print('======================================================')'''
-
-      for link in links:
-          doc = link.text
-          if '--' in doc:
-            index = doc.index('--')
-            #print(index)
-            docs.append(doc[:index])
-          else:
-            docs.append(doc)
-      #print('===============================================')
-      #count += 1
-  #print(docs[107:127])
-  
-  #存爬到的文章用的
-  """with open('ptt_docs8.json', 'w') as outfile:
-            json.dump(docs, outfile)"""
+    scrap()
