9f6b50fb3d432d257f523120eebcc7991f772878
al7771998 <47841102+al7771998@users.noreply.github.com>
2019-06-15 19:16:28 +0800

    Add files via upload
    add main url crawling

--- crawling.py
+++ crawling.py
@@ -4,12 +4,57 @@ import concurrent.futures
 import requests                # This is not standard library
 import json
 from bs4 import BeautifulSoup
-f = open('urlsPttGossip2.json', 'r')
+HTML_PARSER = "html.parser"
+url = 'https://www.ptt.cc/bbs/Gossiping/index.html' ##什麼版可以換
+URLS = []
+for _ in range(1000):
+  if 'Gossiping' in url:
+    r = requests.Session()
+    payload = {
+      "from": "/bbs/Gossiping/index.html",
+      "yes": "yes"
+    }
+    r1 = r.post("https://www.ptt.cc/ask/over18?from=%2Fbbs%2FGossiping%2Findex.html", payload)
+    req = r.get(url)
+
+  elif 'sex' in url:
+    r = requests.Session()
+    payload = {
+      "from": "/bbs/sex/index.html",
+      "yes": "yes"
+    }
+    r1 = r.post("https://www.ptt.cc/ask/over18?from=%2Fbbs%2FGossiping%2Findex.html", payload)
+    req = r.get(url)
+
+  else:
+    req = requests.get(url)
+  soup = BeautifulSoup(req.content, HTML_PARSER)
+  #print(soup)
+  titles = soup.find_all('div', class_='title')
+#print(titles)
+  for t in titles:
+    tag = t.find('a')
+    if tag is None:
+      continue
+    url_str = 'http://www.ptt.cc' + tag.get('href')
+    print(url_str)
+    URLS.append(url_str)
+  page_button = soup.find('div', class_='btn-group btn-group-paging')
+  last_page = page_button.find_all('a', class_='btn wide')[1]
+#print(last_page)
+  url = 'http://www.ptt.cc' + last_page['href']
+#print(URLS)
+
+'''with open ('URLSPttGossip3.json','w') as f:
+  json.dump(URLS,f)'''
+
+'''f = open('URLSPttGossip3.json', 'r')
 URLS = json.load(f)
-f.close()
+f.close()'''
  
 
 def get_content(url):
+  if 'Gossiping' in url:
     r = requests.Session()
     payload = {
         "from": "/bbs/Gossiping/index.html",
@@ -17,12 +62,22 @@ def get_content(url):
     }
     r1 = r.post("https://www.ptt.cc/ask/over18?from=%2Fbbs%2FGossiping%2Findex.html", payload)
     return r.get(url).text
- 
+  elif 'sex' in url:
+    r = requests.Session()
+    payload = {
+        "from": "/bbs/sex/index.html",
+        "yes": "yes"
+    }
+    r1 = r.post("https://www.ptt.cc/ask/over18?from=%2Fbbs%2FGossiping%2Findex.html", payload)
+    return r.get(url).text
+  else:
+    return requests.get(url).text
  
 def scrap():
     docs = []
     with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
         future_to_url = {executor.submit(get_content, url): url for url in URLS}
+        count = 0
         for future in concurrent.futures.as_completed(future_to_url):
             url = future_to_url[future]
             try:
@@ -36,7 +91,10 @@ def scrap():
                 for link in links:
                     doc = link.text
                     docs.append(doc)
-        with open('ptt_gossip_docs.json', 'w') as outfile:
+                    count += 1
+                    
+
+        with open('ptt_gossip_docs2.json', 'w') as outfile:
             json.dump(docs, outfile)
 def main():
     for url in URLS:
@@ -50,3 +108,4 @@ def main():
  
 if __name__ == '__main__':
     scrap()
+    print('finish!')
